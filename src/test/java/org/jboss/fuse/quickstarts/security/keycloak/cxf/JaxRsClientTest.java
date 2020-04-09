dd311be8a5c30049fe81538126cb472db3c3884c
Freeman Fang <freeman.fang@gmail.com>
2020-04-09 17:00:01 -0400

    add schema validation to soap endpoint

--- src/test/java/org/jboss/fuse/quickstarts/security/keycloak/cxf/JaxRsClientTest.java
+++ src/test/java/org/jboss/fuse/quickstarts/security/keycloak/cxf/JaxRsClientTest.java
@@ -245,6 +245,47 @@ public class JaxRsClientTest {
             camelctx.stop();
         }
     }
+    
+    
+@Test
+    
+    public void testRestClientWithSTSInvalidZipCode() throws Exception {
+        
+        String accessToken = fetchAccessToken();
+
+        
+
+        URL resourceUrl = getClass().getResource("/spring/camel-context.xml");
+        CamelContext camelctx = SpringCamelContextFactory.createSingleCamelContext(resourceUrl, null);
+        camelctx.start();
+        try {
+            Assert.assertEquals(ServiceStatus.Started, camelctx.getStatus());
+            
+            Client client = ClientBuilder.newClient().register(JacksonJsonProvider.class).register(LoggingFeature.class);
+            
+                        
+                
+            WeatherRequest request = new WeatherRequest();
+            request.setZipcode("M3H 278");
+            
+            
+            // POST @WeatherPortType#weatherRequest(WeatherRequest)
+            String payload = new ObjectMapper().writeValueAsString(request);
+            
+            
+
+            WeatherResponse response = client.target(JAXRS_URL + "/request").
+                request().header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken).post(Entity.entity(payload, MediaType.APPLICATION_JSON), WeatherResponse.class);
+            Assert.assertEquals("M3H 2H8", response.getZip());
+            Assert.assertEquals("LA", response.getCity());
+            Assert.assertEquals("CA", response.getState());
+            Assert.assertEquals("95%", response.getHumidity());
+            Assert.assertEquals("28", response.getTemperature());           
+
+        } finally {
+            camelctx.stop();
+        }
+    }
 
     private String fetchAccessToken()
         throws UnsupportedEncodingException, IOException, ClientProtocolException {
